be2d2736159467019bd6ffd6430edf43f2f5ceb1
hritik <hchokker7@gmail.com>
2019-04-18 14:16:15 +0530

    fifth commit

--- js/controllers/controller_movie.js
+++ js/controllers/controller_movie.js
@@ -1,11 +1,44 @@
-const movieutil=new MovieUtil;
-const myview = new MovieView;
-document.getElementById('submitmoviebutton').addEventListener('click', (e) => {
-    e.preventDefault();
-    document.getElementById('moviesearch').value == '' ? alert("Enter a value first") : movieFunction();
+const movieUtil = new MovieUtil;
+const movieView = new MovieView;
+// let seasonSearch = document.getElementById('seasonsearch').value;
+// let episodeSearch = document.getElementById('episodesearch').value;
+window.addEventListener('DOMContentLoaded', (e) => {
+    document.getElementById('moviesubmit').addEventListener('click', (e) => {
+        document.getElementById('imdbsearch').value === '' && document.getElementById('namesearch').value === '' && document.getElementById('yearsearch').value ? alert('enter all values') : movieFunction();
+        e.preventDefault();
+    });
 });
 
+
 function movieFunction() {
-    let movieSearch = document.getElementById('moviesearch').value;
-    movieutil.getMovie(imdbsearch,namesearch,season,episode,year);
+    let imdbSearch = document.getElementById('imdbsearch').value;
+    let nameSearch = document.getElementById('namesearch').value;
+    let yearSearch = document.getElementById('yearsearch').value;
+    console.log(imdbSearch, nameSearch, yearSearch);
+    // movieUtil.getMovie(imdbSearch, nameSearch, yearSearch)
+    let output = `
+            <div class="w-100 h-50 embed-responsive embed-responsive-16by9" >
+            <iframe src="https://api.odb.to/embed?imdb_id=${imdbSearch}&title=${nameSearch}&year=${yearSearch}" 
+        allowscriptaccess="always" allowfullscreen="true" scrolling="no" frameborder="0"></iframe>
+        </div>`;
+    document.getElementById('result').innerHTML = output;
 }
+// function movieTitle(imdbSearch, nameSearch, yearSearch){
+//     console.log(imdbSearch, nameSearch, yearSearch);
+//     movieutil.getAllMovie();
+// }
+
+//  ? alert("Enter a value first") : movieFunction();
+
+
+
+//     if (imdbSearch === '' && nameSearch === '' && yearSearch === ''){
+//         console.log(imdbSearch, nameSearch, yearSearch);
+//         movieUtil.getMovie(imdbSearch, nameSearch, yearSearch);
+//     }
+//     else if (document.getElementById('imdbsearch').value == '' && document.getElementById('namesearch').value == '' && document.getElementById('yearsearch').value == '' && document.getElementById('seasonsearch').value == '' && document.getElementById('episodesearch').value == '' ){
+//         console.log(imdbSearch, nameSearch, yearSearch, seasonSearch, episodeSearch);
+//         movieUtil.getAllMovie(imdbSearch, nameSearch, yearSearch, seasonSearch, episodeSearch);
+//     }
+//     e.preventDefault();
+// });
--- js/utils/movieutil.js
+++ js/utils/movieutil.js
@@ -1,5 +1,12 @@
-const MovieUtil{
+class MovieUtil{
     constructor(){
       
+    }
+  async  getMovie(imdbSearch, nameSearch, yearSearch) {
+        const fullMovie = await fetch(`https://api.odb.to/embed?imdb_id=${imdbSearch}&title=${nameSearch}&year=${yearSearch}`);
+
+        const watchMovie=await fullMovie.json({});
+    
+        return watchMovie;
     }
 }
